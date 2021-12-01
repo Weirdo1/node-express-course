9dee967d591ee52d148b0220501fdc5a842d629d
wss <1104112583@qq.com>
2021-12-01 17:59:35 +0800

    add first GET route

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const app = express();
 app.listen(8000,function(){
 	console.log("server is running")
 })
+
+const mockUserData = [
+	{
+		name :'mack', 
+		
+	},
+	{
+		name :'jecks', 
+		
+	}
+];
+app.get('/user',function(req,res){
+	res.json({
+		success:true,
+		message:"success",
+		users:mockUserData
+	})
+})
