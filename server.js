25859ec6a2ca91d29d63efe9735216fefaa84630
wss <1104112583@qq.com>
2021-12-02 16:12:19 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.listen(8000, function () {
-  console.log("正在监听8000端口。。。");
-});
+const app = express();
+app.use(bodyParser.json());
+
+
 
 const mockUserData = [
   { 
@@ -30,4 +31,28 @@ app.get("/users/:id", function (req, res) {
     message: "got one user",
     user: req.params.id,
   });
+});
+
+app.post("login", function (req, res) {
+  const userName = req.body.username;
+  const password = req.body.password;
+  const mockUserName = "bbb";
+  const mockPassword = "111";
+  
+  if(userName === mockUserName && password === mockPassword ) {
+    res.json({
+      success: true,
+      message: "match",
+      token: "111",
+    });
+  }else{
+    res.json({
+      success: false,
+      message: "do not match",
+    });
+  }
+});
+
+app.listen(8000, function () {
+  console.log("正在监听8000端口。。。");
 });
