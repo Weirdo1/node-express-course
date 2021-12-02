2d29e1a17a255fba82bd24c920ea5b53d4c54ae8
wss <1104112583@qq.com>
2021-12-02 15:28:57 +0800

    add dynamic GET route

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(8000,function(){
-	console.log("正在监听8000端口。。。")
-})
+app.listen(8000, function () {
+  console.log("正在监听8000端口。。。");
+});
 
 const mockUserData = [
-	{
-		name :'mack', 
+  { 
+    name: "mack", 
 		
-	},
-	{
-		name :'jecks', 
+  },
+  {
+    name: "jecks", 
 		
-	}
+  },
 ];
-app.get('/user',function(req,res){
-	res.json({
-		success:true,
-		message:"success",
-		users:mockUserData
-	})
-})
+app.get("/user", function (req, res) {
+  res.json({
+    success: true,
+    message: "success",
+    users: mockUserData,
+  });
+});
 
-app.get('/users/:id',function(req,res){
-	console.log(req.params.id)
-	res.json({
-		success: true,
-		message: 'got one user',
-		user: req.params.id
-	})
-})
+app.get("/users/:id", function (req, res) {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    message: "got one user",
+    user: req.params.id,
+  });
+});
